4e4e77244a2e0f9e787cccc3ab80519393ecabaf
charmingruby <mrrubycanal3105@gmail.com>
2022-04-26 13:57:18 -0300

    finished

--- react-navigation/src/components/layout/Content.jsx
+++ react-navigation/src/components/layout/Content.jsx
@@ -5,13 +5,15 @@ import { Routes, Route } from 'react-router-dom'
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
 import Param from '../../views/examples/Param'
+import NotFound from '../../views/examples/NotFound'
 
 const Content = props => (
     <main className='Content'>
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/param/:id" element={<Param/>}/>
+            <Route exact path="/about" element={<About/>}/>
+            <Route exact path="/param/:id" element={<Param/>}/>
+            <Route exact path="*" element={<NotFound/>}/>
+            <Route exact path="/" element={<Home/>}/>
         </Routes>
     </main>
 )
